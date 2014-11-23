ff71fd5f44fe2ef9e08b2fcdd106889d45b47b6f
Richard Branson <rmbranson@gmail.com>
2014-11-23 19:51:46 +0000

    Added get_name method with a sanity check of longer than 2 letter responses

---
+++ lib/name.rb
@@ -0,0 +1,22 @@
+# declare new method
+# ask for user input
+# return user input and remove extra white space and line return
+# check if user enters a proper name. if less than 2 letters ask if that is their name.
+
+def ask_name
+	puts "=" * 5 + " What's your name?" + "." *  57
+	name = gets.chomp
+	if name.length <= 2
+		puts "=" * 5 + " Humm... seems a it short. Are you sure that is your name? (Y/N)"
+		name_confirm = gets.chomp
+			if name_confirm.downcase == "y"
+				puts "=" * 5 + " Great, #{name}! Let's get started."
+			else name_confirm.downcase == "n"
+					puts "=" * 5 + " Well if you are not going to give me your name I'm going to call you Richard Branson for now!"
+					name = "Richard Branson"
+			end
+	else
+		puts "=" * 5 + " Great, #{name}! Let's get started."
+	end
+	return name
+end
--- main.rb
+++ main.rb
@@ -6,17 +6,22 @@
 # Push code on Github.
 ######### SOLUTION #########
 
+require_relative 'lib/name'
+
 ######### GET USER INPUT #########
 system "clear"
 puts "=" * 80
 puts "=" * 30 + " Welcome to YELP-RB " + "=" * 30
-puts "=" * 6 + " A terminal app that queries the api.yelp.com to find local stuff. " + "=" * 7
+puts "=" * 5 + " A terminal app that queries the api.yelp.com to find local stuff. " + "=" * 8
 puts "=" * 80
 puts "=" * 80
 sleep 1
-puts "=" * 5 + " You can search for businesses by keywords that are local to you. " + "=" * 9
+puts "=" * 5 + " You can search for local businesses by keywords."
+sleep 2
+puts "=" * 5 + " but first..."
 sleep 1
-puts "=" *  5 + " but first... " + "=" *  61
 user = ask_name # Define "ask_name" method in new file called name.rb.
+
+
 ######### SERACH YELP API #########
 ######### DISPLAY RESULTS TO USER #########
